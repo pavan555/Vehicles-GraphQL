3f049470223722471ea98b14d2f8b2d540eb45dc
pavan555 <gandhipavan555@gmail.com>
2025-06-04 09:07:44 +0530

    rules added partially --> still need to know

--- 2. Rules/initial-project/index.js
+++ 2. Rules/initial-project/index.js
@@ -2,15 +2,59 @@ import {ApolloServer} from "@apollo/server";
 import {startStandaloneServer} from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
-  type Query {
-    cars: [Car!]!
-  }
 
   type Car {
     id: ID!
     color: String!
     make: String!
+    engineType: String!
   }
+
+
+ type Group {
+   id: ID!,
+   name: String!,
+   description: String,
+   image: Image,
+   cars(skip: Int = 0, offset:Int = 10): [Car!]!,
+   autoGroupFeatures: AutoGroupFeatures,
+ }
+
+  type Image{
+    id: ID!,
+    url: URL!
+  }
+
+  type AutoGroupFeatures {
+    features: [GroupFeature!]!
+    applyFeatures: [ApplyFeatureCondition!]!
+  }
+
+  enum ApplyFeatureCondition {
+    ALL
+    ANY
+  }
+
+  enum GroupFeature{
+    ELECTRIC
+    HYBRID
+    DIESEL
+    PETROL
+    TURBO
+    MANUAL_GEAR
+    AUTOMATIC_GEAR
+  }
+
+  type Query {
+    cars: [Car!]!
+    groups: [Group!]!
+    group(id: ID!): Group
+  }
+    type Mutation{
+        addCar(color: String!, make: String!, engineType: String!): Car!
+        addGroup(name: String!, description: String): Group!
+        addCarToGroup(groupId: ID!, carId: ID!): Group!
+    }
 `;
 
 const server = new ApolloServer({
